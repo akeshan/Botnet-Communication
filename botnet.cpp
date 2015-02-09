01d8426acfd0ee7db7782cf4a2e22d7074af3b84
Zeeshan <pirzadaza@gmail.com>
2015-02-08 20:37:55 -0800

    integrated botnet and server

--- botnet.cpp
+++ botnet.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <string>
 #include <cstdlib> 
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 
 using namespace std;
 
@@ -21,28 +25,9 @@ int randomFunctionGenerator(int currentTimeInMinutes, string seed) {
     return abs(hash) % 7; // mod the absolute value of the hash with the number of functions we have
 }
 
-void a(){
-    cout << "I am waiting to receive packets, decode the speed field, and run attack a." <<endl;
-}
-void b(){
-    cout << "I am waiting to receive packets, decode the latitude field, and run attack b." <<endl;
-}
-void c(){
-    cout << "I am waiting to receive packets, decode the longitude field, and run attack c." <<endl;
-}
-void d(){
-    cout << "I am waiting to receive packets, decode some field, and run attack d." <<endl;
-}
-void e(){
-    cout << "I am waiting to receive packets, decode some field, and run attack e." <<endl;
-}
-void f(){
-    cout << "I am waiting to receive packets, decode some field, and run attack f." <<endl;
-}
-void g(){
-    cout << "I am waiting to receive packets, decode some field, and run attack g." <<endl;
-}
 
+
+#define LEN 1024
 
  int main(int argc, char *argv[])
  {
@@ -51,41 +36,91 @@ void g(){
     bool firstRun = true;
     int runEvery = 1; // pick a new function randomly every X minutes
 
+    //network stuff
+    int sockfd;
+    struct sockaddr_in server;
+    char buff[LEN];
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(9034);
+
+    connect(sockfd, (struct sockaddr *)&server, sizeof(server));
+
     for(;;) {
+        bzero(buff, LEN);
+        
         int currentTimeInMinutes = (time(0)/60) % 60;
 
         if(minutes + runEvery == currentTimeInMinutes || firstRun == true) {
             minutes = currentTimeInMinutes;
             int function = randomFunctionGenerator(currentTimeInMinutes, globalSeed);
 
+           
             switch (function) {
-                case 0: 
-                    a();
+                case 0:
+                {
+                   char message0[] = "Message using A\n";
+                   send(sockfd, message0, strlen(message0), 0);
+                   recv(sockfd, buff, LEN, 0);
+                   puts(buff);
+                }
+                   break;
+                case 1:
+                {
+                   char message1[] = "Message using B\n";
+                   send(sockfd, message1, strlen(message1), 0);
+                   recv(sockfd, buff, LEN, 0);
+                   puts(buff);
+                }
                     break;
-                case 1: 
-                    b();
+                case 2:
+                {
+                   char message2[] = "Message using C\n";
+                   send(sockfd, message2, strlen(message2), 0);
+                   recv(sockfd, buff, LEN, 0);
+                   puts(buff);
+                }
                     break;
-                case 2: 
-                    c();
+                case 3:
+                {
+                    char message3[] = "Message using D\n";
+                   send(sockfd, message3, strlen(message3), 0);
+                   recv(sockfd, buff, LEN, 0);
+                   puts(buff);
+                }
                     break;
-                case 3: 
-                    d();
+                case 4:
+                {
+                    char message4[] = "Message using E\n";
+                    send(sockfd, message4, strlen(message4), 0);
+                    recv(sockfd, buff, LEN, 0);
+                    puts(buff);
+                }
                     break;
-                case 4: 
-                    e();
+                case 5:
+                {
+                    char message5[] = "Message using F\n";
+                    send(sockfd, message5, strlen(message5), 0);
+                    recv(sockfd, buff, LEN, 0);
+                    puts(buff);
+                }
                     break;
-                case 5: 
-                    f();
+                case 6:
+                {
+                    char message6[] = "Message using G\n";
+                    send(sockfd, message6, strlen(message6), 0);
+                    recv(sockfd, buff, LEN, 0);
+                    puts(buff);
+                }
                     break;
-                case 6: 
-                    g();
-                    break;
+                  
                 default: 
-                    cout << "YOU MESSED UP YOUR RANDOM FUNCTION GENERATOR" << endl;
                     break;
             }
-            cout << endl;
-        }
+            
+        } 
         firstRun = false;
     }
 
