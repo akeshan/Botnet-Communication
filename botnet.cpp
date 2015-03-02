6b3a454743c957885bd40d99615cad8f61c16e72
Zeeshan <pirzadaza@gmail.com>
2015-03-02 12:52:10 -0800

    synch

--- botnet.cpp
+++ botnet.cpp
@@ -1,11 +1,14 @@
 #include <ctime>
 #include <iostream>
-#include <string>
-#include <cstdlib> 
-#include <string.h>
+#include <string> 
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
+#include "Matrix.h"
 
 using namespace std;
 
@@ -29,8 +32,28 @@ int randomFunctionGenerator(int currentTimeInMinutes, string seed) {
 
 #define LEN 1024
 
- int main(int argc, char *argv[])
+
+void randomString(char *buff, int len)
+{
+   static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; ++i) {
+        buff[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+
+    buff[len] = 0;
+}
+
+int main(int argc, char *argv[])
  {
+    //since listener thread needs function
+ //   int function = randomFunctionGenerator((time(0)/60)%60, globalSeed);
+
+    srand (time(NULL));
+
     time_t t = time(0);   // get time now
     int minutes = ((t / 60) % 60); // current time in minutes
     bool firstRun = true;
@@ -48,8 +71,29 @@ int randomFunctionGenerator(int currentTimeInMinutes, string seed) {
 
     connect(sockfd, (struct sockaddr *)&server, sizeof(server));
 
-    for(;;) {
-        bzero(buff, LEN);
+    //create thread that listens for incoming data
+    int pid = fork();
+    if (pid < 0 )
+    {
+        printf("Fork failed\n");
+    }
+    if (pid == 0) 
+    {
+        for (;;)
+        {
+           // printf("Got here");
+            int currentTimeInMinutes = (time(0)/60) % 60;
+            int function = randomFunctionGenerator(currentTimeInMinutes, globalSeed);
+            bzero(buff, LEN);
+            int a = recv(sockfd, buff, LEN, 0);
+            printf("Received Message: %s using encoding scheme: %d \n", buff, function);
+         //   puts(buff);
+        }
+    }
+
+    // write data to server
+    for(;;) 
+    {
         
         int currentTimeInMinutes = (time(0)/60) % 60;
 
@@ -57,72 +101,22 @@ int randomFunctionGenerator(int currentTimeInMinutes, string seed) {
             minutes = currentTimeInMinutes;
             int function = randomFunctionGenerator(currentTimeInMinutes, globalSeed);
 
-           
-            switch (function) {
-                case 0:
-                {
-                   char message0[] = "Message using A\n";
-                   send(sockfd, message0, strlen(message0), 0);
-                   recv(sockfd, buff, LEN, 0);
-                   puts(buff);
-                }
-                   break;
-                case 1:
-                {
-                   char message1[] = "Message using B\n";
-                   send(sockfd, message1, strlen(message1), 0);
-                   recv(sockfd, buff, LEN, 0);
-                   puts(buff);
-                }
-                    break;
-                case 2:
-                {
-                   char message2[] = "Message using C\n";
-                   send(sockfd, message2, strlen(message2), 0);
-                   recv(sockfd, buff, LEN, 0);
-                   puts(buff);
-                }
-                    break;
-                case 3:
-                {
-                    char message3[] = "Message using D\n";
-                   send(sockfd, message3, strlen(message3), 0);
-                   recv(sockfd, buff, LEN, 0);
-                   puts(buff);
-                }
-                    break;
-                case 4:
-                {
-                    char message4[] = "Message using E\n";
-                    send(sockfd, message4, strlen(message4), 0);
-                    recv(sockfd, buff, LEN, 0);
-                    puts(buff);
-                }
-                    break;
-                case 5:
-                {
-                    char message5[] = "Message using F\n";
-                    send(sockfd, message5, strlen(message5), 0);
-                    recv(sockfd, buff, LEN, 0);
-                    puts(buff);
-                }
-                    break;
-                case 6:
-                {
-                    char message6[] = "Message using G\n";
-                    send(sockfd, message6, strlen(message6), 0);
-                    recv(sockfd, buff, LEN, 0);
-                    puts(buff);
-                }
-                    break;
-                  
-                default: 
-                    break;
-            }
-            
+
+            char msg[LEN];
+            int n = rand()%10 + 1;
+            randomString(msg, n);
+            printf("Sending Message: %s using encoding scheme %d\n", msg, function);
+            send(sockfd, msg, strlen(msg), 0);
+                
         } 
         firstRun = false;
     }
 
     return 0;
  }
+
+
+
+
+
+
