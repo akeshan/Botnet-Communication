e5299b4500560b2a87b1e00448938ac4d3e7d9ea
trevoranderson <trevora@ucla.edu>
2015-03-14 14:55:19 -0700

    Switched to std::thread from fork() and fixed bugs

---
+++ Matrix.h
@@ -0,0 +1,285 @@
+//file name: Matrix3.cpp
+//complie: g++ -o A Matrix2.cpp
+//run:     ./A
+//date: 3/2/15
+#include <iostream>
+using namespace std;
+
+struct MatrixBank
+{
+ //This part is complete now
+ int bank3[7][20] = { {1, -1, 1,   -2, 1, -1,   2, 3, -4 ,0,0,0,0,0,0,0,0,0,0,0},
+                      {2,  3, 2,    3, 3,  4,   1, 1,  1 ,0,0,0,0,0,0,0,0,0,0,0},
+                      {1,  1, 1,    3, 4,  3,   3, 3,  4 ,0,0,0,0,0,0,0,0,0,0,0},
+                      {-1, -1, 0,  -10, -6, -1, -8, -5, -1 ,0,0,0,0,0,0,0,0,0,0,0},
+                      {-1, -1, 6,   1, 0, -2,   0, 1, -3 ,0,0,0,0,0,0,0,0,0,0,0},
+                      {7, -1, -1,   -3, 1, 0,   -3, 0, 1 ,0,0,0,0,0,0,0,0,0,0,0},
+                      {2, -1, 0,   -17, 11, 3,   11, -7, -2 ,0,0,0,0,0,0,0,0,0,0,0}                    
+                    };
+char mapping[37] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+                  'r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
+
+ int bank4[7][20] = { 
+                     // I am working on size 4 by 4
+                    };
+}b;
+
+class Encode {
+public:
+
+Encode(char arr[], int field, int random)
+{
+       m_size = 3;
+       m_field = field;
+       
+       m_original = new char[m_size];
+       for (int i = 0; i < m_size; i++){
+          m_original[i] = arr[i];
+       }
+       m_message = new int[m_size];
+       for (int i = 0; i < m_size; i++){
+         if ( arr[i] ==' ')
+             m_message[i] = 0;
+         else
+            if ( arr[i]<='z' && arr[i]>= 'a')
+                m_message[i] = arr[i]-'a'+1;
+            else
+               if (arr[i]<='9' && arr[i]>='0')
+                   m_message[i] = arr[i]-'0'+27;
+               else  m_message[i] = 0;
+       }
+
+       if ( (random > 6) || (random <0) )
+          random = 6;
+       bMatrix = b.bank3[random];
+
+        // Display selected matrix  
+ //       cout << " selected matrix : ";
+   //     for (int i = 0; i < 20; i++) 
+  //         cout << bMatrix[i] << " ";
+   //     cout << "\n";
+
+       
+}
+
+void printMessage()
+{
+        // Display original message   
+        cout << " Original Message : ";
+        for (int i = 0; i < m_size; i++) 
+           cout << m_original[i] << " ";
+        cout << "\n";
+}
+
+
+void encoding(int *encodedMessage) {
+                              
+        //encoding and output results
+  //      cout << " Encoded  Message : ";
+        for (int col = 0; col < m_size; col++) {
+            
+            for (int inner = 0; inner < m_size; inner++) {
+//                cout << encodedMessage[col] << " += " << m_message[inner] << " * " << bMatrix[inner+col*m_size] << endl;
+                encodedMessage[col] += m_message[inner] * bMatrix[inner+col*m_size];
+            }
+ //           cout << encodedMessage[col] << "  " << endl;
+        }
+//        cout << "\n";
+}
+
+~Encode ()
+{
+ delete [] m_message;
+ delete [] m_original;
+}
+
+private:
+
+char *m_original;
+int *m_message;
+int  m_size;
+int  m_field;
+int *bMatrix;
+};
+
+
+struct MatrixInverseBank
+{
+ // This part is comp;ete now
+ int bankInverse3[7][20] = {{-1, -1,  0,   -10, -6, -1,   -8, -5, -1 ,0,0,0,0,0,0,0,0,0,0,0},
+                            {-1, -1,  6,     1,  0, -2,    0,  1, -3 ,0,0,0,0,0,0,0,0,0,0,0},
+                            { 7, -1, -1,    -3,  1,  0,   -3,  0,  1 ,0,0,0,0,0,0,0,0,0,0,0},
+                            {1, -1, 1,   -2, 1, -1,   2, 3, -4 ,0,0,0,0,0,0,0,0,0,0,0},
+                            {2, 3, 2,   3, 3, 4,   1, 1, 1 ,0,0,0,0,0,0,0,0,0,0,0},
+                            {1, 1, 1,   3, 4, 3,   3, 3, 4 ,0,0,0,0,0,0,0,0,0,0,0},
+                            {1, 2, 3,   1, 4, 6,   2, -3, -5 ,0,0,0,0,0,0,0,0,0,0,0}
+                    
+                          };
+ char mapping[37] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+                   'r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
+
+ int bankInverse4[7][20] = { 
+                            //I am working on size 4 by 4
+                            };
+
+}bi;
+
+
+class Decode {
+public:
+
+Decode(int arr[], int field, int random)
+{
+       m_size = 3;
+       m_field = field;
+       
+       m_message = new int[m_size];
+       for (int i = 0; i < m_size; i++) 
+           m_message[i] = arr[i];
+
+       temp = new int[m_size];
+       for (int i = 0; i < m_size; i++) 
+           temp[i] = 0;
+       
+
+       if( (random > 6) || (random <0) )
+          random = 6;
+
+       bMatrix = bi.bankInverse3[random];
+
+        // Display selected matrix  
+  //      cout << " selected matrix : ";
+    //    for (int i = 0; i < 20; i++) 
+      //     cout << bMatrix[i] << " ";
+      //  cout << "\n";
+
+       
+}
+
+void printMessage()
+{
+        // Display original message   
+        cout << " Original Message : ";
+        for (int i = 0; i < m_size; i++) 
+           cout << m_message[i] << " ";
+        cout << "\n";
+}
+
+
+void decoding(char *decodedMessage) {
+                              
+        //encoding and output results
+   //     cout << " decoded  Message : ";
+        for (int col = 0; col < m_size; col++) {
+            
+            for (int inner = 0; inner < m_size; inner++) {
+                temp[col] += m_message[inner] * bMatrix[inner+col*m_size];
+                
+            }
+            decodedMessage[col] = bi.mapping[temp[col]];
+  //          cout << decodedMessage[col] << "  ";
+        }
+//        cout << "\n";
+}
+
+~Decode ()
+{
+ delete [] m_message;
+ delete [] temp;
+}
+
+private:
+
+int *m_message;
+int *temp;
+int  m_size;
+int  m_field;
+int *bMatrix;
+};
+
+
+/*
+
+
+void encodeBatch(char *res, char *src, int n, int field, int func)
+{
+  int result[n];
+
+  for (int i = 0; i < n; i++) 
+    result[i] = 0;
+
+
+  Encode e(src, field, func);
+  e.encoding(result); //result now is encoded in int form
+
+
+  //copy into res
+  for (int i = 0; i < n; i++)
+  {
+      res[i] = (char)result[i];
+  }
+
+}
+
+void encodeGenericString(char *res, char *src, int n, int field, int func)
+{
+    
+    for (int i = 0; i < n; i += 3)
+    {
+      encodeBatch(res + i, src + i, 3, field, func);
+    }
+
+}
+
+void decodeBatch(char *res, char *src, int n, int field, int func)
+{
+  int messageI[n];  //holds decoded message
+  
+  for (int i = 0; i < n; i++)
+  {
+      messageI[i] = (int)src[i];
+  }
+
+  Decode d(messageI, field, func);
+  d.decoding(res);
+
+}
+
+void decodeGenericString(char *res, char *src, int n, int field, int func)
+{
+    for (int i = 0; i < n; i += 3)
+    {
+        decodeBatch(res + i, src + i, 3, field, func);
+    }
+}*/
+/*
+int main() {
+      char res[10];
+      char *src = "lk";
+      encodeGenericString(res, src, 7, 3, 1);
+
+      char end[10];
+      decodeGenericString(end, res, 7, 3, 1);
+
+      printf("%s\n", end);
+      return 0;   
+};
+
+
+/*
+    char message[3] = {'k', 'l', 'd'}; // message you want to encode
+    int result[3] = {0,0,0}; //to hold encoded message
+    Encode e(message,3,0); //message: message to encode, 3: field, 5: random number to pick a matrix
+    e.printMessage();
+    e.encoding(result); //pass in a same size matrix to get result
+
+    int messageI[3] ={3,-14,42}; // message you want to decode
+
+    char resultI[3] = {' ',' ',' '};//to hold decoded message
+
+
+    Decode d(messageI,3,0);//messageI :message to decode, 3: field, 5: random number to pick a Inverse of a matrix
+
+    d.decoding(resultI);//pass in a same size matrix to get result
+*/
+ 
--- botnet.cpp
+++ botnet.cpp
@@ -3,41 +3,44 @@
 #include <sys/shm.h> 
 #include <ctime>
 #include <iostream>
-#include <string> 
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <string>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
 #include <csignal>
-
+#include <thread>
+#include <mutex>
 #include "Matrix.h"
-
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define LEN 1024
+#define RUNEVERY 1
 using namespace std;
 
 string globalSeed = "1234";
+ int sockfd;
+char buff[LEN];
 
+// Locks fun to prevent race conditions.
+mutex funmux;
+int *fun;
 
 int randomFunctionGenerator(int currentTimeInMinutes, string seed) {
     srand(stoi(seed));
     int hash = currentTimeInMinutes + rand()%100; //s current time in minutes + random offset based on seed
-
     int offset = 'a' - 1;
     for(string::const_iterator it=seed.begin(); it!=seed.end(); ++it) {
         hash = hash << 1 ^ (*it - offset);
     }
-
     globalSeed = to_string(hash); // use current hash as the next seed
-
     return abs(hash) % 7; // mod the absolute value of the hash with the number of functions we have
 }
-
-
-
-#define LEN 1024
-
-
 void randomString(char *buff, int len)
 {
    static const char alphanum[] =
@@ -48,37 +51,81 @@ void randomString(char *buff, int len)
     for (int i = 0; i < len; ++i) {
         buff[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
     }
-
+    // len is significantly less than LEN so this will not cause a segfault unless len is 0
     buff[len] = 0;
 }
-
 void segfault_sigaction(int signal, siginfo_t *si, void *arg)
 {
     printf("Caught segfault at address %p\n", si->si_addr);
     exit(0);
 }
+void syncthread()
+{
+        time_t t = time(0);   // get time now
+        int minutes = ((t / 60) % 60); // current time in minutes
+        bool firstRun = true;
+        
+        for(;;)
+        {
+            int currentTimeInMinutes = (time(0)/60) % 60;
+            if(minutes + RUNEVERY == currentTimeInMinutes || firstRun) 
+            {
+                minutes = currentTimeInMinutes;
+		funmux.lock();
+                *fun = randomFunctionGenerator(currentTimeInMinutes, globalSeed);
+		funmux.unlock();
+                if (firstRun == true)
+                    printf("Addr of function: %p\n", fun);
+            }    
+            firstRun = false;
+        }
+}
+void listenthread()
+{
+      for (;;)
+        {
+		int locfun;
+		funmux.lock();
+		locfun = *fun;
+      		funmux.unlock();
+            //bzero(buff, LEN);
+		
+            int a = recv(sockfd, buff, LEN, 0);
+            printf("Recieved Message: %s ", buff);
+            printf("using encoding scheme: %d \n", locfun);     
+        }
+}
 
-
-
+void serverthread()
+{
+ for(;;) 
+    {
+        // shared memory stuff
+        sleep(rand()%20 + 5);
+        //this code is for writing data
+        char msg[LEN];
+        int n = rand()%10 + 1;
+        randomString(msg, n);
+        send(sockfd, msg, strlen(msg), 0);
+    }
+}
 int main(int argc, char *argv[])
  {
-    
-    
+    fun = new int;
+    *fun = 0;
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
     sigemptyset(&sa.sa_mask);
     sa.sa_sigaction = segfault_sigaction;
     sa.sa_flags   = SA_SIGINFO;
-
+	
     sigaction(SIGSEGV, &sa, NULL);
 
     srand (time(NULL));
 
-
     //network stuff
     int sockfd;
     struct sockaddr_in server;
-    char buff[LEN];
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -86,96 +133,13 @@ int main(int argc, char *argv[])
     server.sin_port = htons(9034);
 
     connect(sockfd, (struct sockaddr *)&server, sizeof(server));
-
-
-
-    //create thread that handles synchronization stuff
-    int pid2 = fork();
-    if (pid2 == 0)
-    {
-
-        // shared memory stuff
-        key_t key = 5678;
-        int shmid;
-        int *function;
-
-        if ((shmid = shmget(key, sizeof(int), IPC_CREAT|0666)) < 0)
-        {
-            perror("shmget");
-            exit(1);
-        }
-
-        function = (int *)shmat(shmid, NULL, 0);
-    
-
-        time_t t = time(0);   // get time now
-        int minutes = ((t / 60) % 60); // current time in minutes
-        bool firstRun = true;
-        int runEvery = 1; // pick a new function randomly every X minutes
-   
-
-        
-        for(;;)
-        {
-            int currentTimeInMinutes = (time(0)/60) % 60;
-            if(minutes + runEvery == currentTimeInMinutes || firstRun == true) 
-            {
-                minutes = currentTimeInMinutes;
-                *function = randomFunctionGenerator(currentTimeInMinutes, globalSeed);
-                if (firstRun == true)
-                    printf("Addr of function: %p\n", function);
-            }    
-            firstRun = false;
-        }
-
-    }
-    
-
-    //create thread that listens for incoming data
-    int pid = fork();
-    if (pid < 0 )
-    {
-        printf("Fork failed\n");
-    }
-    if (pid == 0) 
-    {
-        for (;;)
-        {
-            key_t key = 5678;
-            int shmid = shmget(key, sizeof(int), 0666);
-            int* function;
-            function = (int *)shmat(shmid, NULL, 0666);
-
-            bzero(buff, LEN);
-            int a = recv(sockfd, buff, LEN, 0);
-            printf("Recieved Message: %s ", buff);
-            printf("using encoding scheme: %d \n", *function);
-            
-     
-        }
-    } 
-
-    // write data to server
-    for(;;) 
-    {
-        // shared memory stuff
-        key_t key = 5678;
-        int shmid = shmget(key, sizeof(int), 0666);
-        int* function;
-        function = (int *)shmat(shmid, NULL, 0666);
-  //      printf("%d\n", *function);
-        sleep(rand()%20 + 5);
-
-        //this code is for writing data
-      
-        char msg[LEN];
-        int n = rand()%10 + 1;
-        randomString(msg, n);
-  //      printf("Sending Message: %s using encoding scheme %d\n", msg, *function);
-        send(sockfd, msg, strlen(msg), 0);
-            
-    }
-
+    thread tsync(syncthread);
+    thread tlisten(listenthread);
+    thread tserver(serverthread);
+// This just blocks main from exiting and killing the threads. The threads won't actually terminate on their own
+    tsync.join();
+    tlisten.join();
+    tserver.join();
     return 0;
  }
 
