1236a6f210a57530d33f4b26f8595e470f762d75
Zeeshan <pirzadaza@gmail.com>
2015-03-15 18:48:36 -0700

    open.sh

--- open.sh
+++ open.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+g++ -g -std=c++11 -pthread botnet.cpp -o botnet
+gcc server.c -o server
+./server &
 xterm -hold -title "botnet1" -e "./botnet 7" &
 xterm -hold -title "botnet2" -e "./botnet 8" &
 xterm -hold -title "botnet3" -e "./botnet 3" &
 xterm -hold -title "botnet4" -e "./botnet 4" &
+
